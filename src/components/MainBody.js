4fdff626e2b23e8fa0a10057325098920fcdb02b
Juan Bonami <Juanbonami@gmail.com>
2020-07-01 23:33:13 -0500

    formatting and importing image

--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react'
 import { Grid, Image } from 'semantic-ui-react'
+import profilepic from '../images/yaboi.jpg';
 
 export class MainBody extends Component {
     render() {
         return (
             <div>
                 <Grid>
-    <Grid.Row>
-      <Grid.Column width={3}>
-        <Image src />
-      </Grid.Column>
-      <Grid.Column width={13}>
-        <Image src />
-      </Grid.Column>
-    </Grid.Row>
-  </Grid>
+                    <Grid.Row>
+                        <Grid.Column width={3}>
+                            <Image src={profilepic} />
+                        </Grid.Column>
+
+                        <Grid.Column width={13}>
+                            <Image src />
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
             </div>
         )
     }
