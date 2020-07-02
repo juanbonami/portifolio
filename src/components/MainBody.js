f04fe2a74702e790ffc11aac9edfff6e8cb5286b
Juan Bonami <Juanbonami@gmail.com>
2020-07-02 00:20:07 -0500

    moved grid components around  and added p tag with dummy text for now

--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -5,17 +5,17 @@ import profilepic from '../images/yaboi.jpg';
 export class MainBody extends Component {
     render() {
         return (
-            <div>
-                <Grid>
-                    <Grid.Row>
-                        <Grid.Column width={3}>
-                            <Image src={profilepic} />
-                        </Grid.Column>
+            <div style={{border: 'solid', }}>
+                <Grid celled='internally' centered columns={2}>
+                    <Grid.Column width={3}>
+                        <Image circular src={profilepic} />
+                    </Grid.Column>
+                </Grid>
 
-                        <Grid.Column width={13}>
-                            <Image src />
-                        </Grid.Column>
-                    </Grid.Row>
+                <Grid celled='internally' centered>
+                    <Grid.Column width={13}>
+                        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+                    </Grid.Column>                  
                 </Grid>
             </div>
         )
