14198350f14e93aeeca80790bb5a0e592220d8c8
Juan Bonami <Juanbonami@gmail.com>
2020-07-02 01:25:57 -0500

    added handleItemClick function

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Menu } from 'semantic-ui-react'
 export class Header extends Component {
     state = {}
 
-  
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
   render() {
     
