6eefba527c2d5ac72c72b6505cb38d758caff656
Juan Bonami <Juanbonami@gmail.com>
2020-07-02 20:34:35 -0500

    added component prop to set Route

--- src/App.js
+++ src/App.js
@@ -6,15 +6,18 @@ import {
 } from "react-router-dom";
 import MainBody from './components/MainBody';
 import Header from './components/Header';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
 
       <Router>
-        <Route exact path="/about" component />
-        <Route exact path="/projects" component />
-        <Route exact path="/contact" component />
+        <Header/>
+        <Route exact path="/about" component={MainBody} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/contact" component={Contact} />
       </Router>
 
       <Header/>
